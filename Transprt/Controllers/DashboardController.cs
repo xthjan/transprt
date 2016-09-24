194fb683bfd13924526474f5e11bdf7f1dc779bd
Michel Castillo <mcastillo@cswsolutions.com>
2016-09-24 00:38:33 -0500

    Untracked Files

---
+++ Transprt/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Transprt.Controllers {
+    public class DashboardController : Controller
+    {
+        [HttpGet]
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
